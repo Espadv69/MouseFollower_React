97b589d4681545eb84a353b13793fcb83e001928
espadv69 <aitorjob69@gmail.com>
2024-08-03 12:49:16 +0200

    Update: - Toggle button to know how many render do this App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+
 import { FollowMouse } from './components/FollowMouse'
 
 function App() {
+  const [mounted, setMounted] = useState(true)
+
   return (
     <>
-      <FollowMouse />
+      { mounted && <FollowMouse /> }
+      <button style={{ marginTop: 10 }} onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse component
+      </button>
     </>
   )
 }
