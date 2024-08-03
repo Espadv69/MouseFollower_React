f0334582a1f50ce58997ecf9e0cbff8cc908f08e
espadv69 <aitorjob69@gmail.com>
2024-08-03 10:14:30 +0200

    Update: - useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('efect ', {enabled})
+  }, [enabled])
+
   return (
-    console.log("Hola mundo")
+    <main>
+
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#00f6ff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }} />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} puntero
+      </button>
+
+    </main>
   )
 }
 
